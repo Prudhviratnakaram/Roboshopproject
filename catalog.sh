cd3e5539fa29f3bf56d4e3c2f657908e26882481
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-28 16:06:19 -0500

    Adding topics header

--- catalog.sh
+++ catalog.sh
@@ -5,6 +5,7 @@
 
  curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
  cd /home/roboshop
+ rm -rf catalog
  unzip -o /tmp/catalogue.zip
  mv catalogue-main catalogue
  cd /home/roboshop/catalogue
