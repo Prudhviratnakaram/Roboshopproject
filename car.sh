fe125a7bad79eaaccf1b180029c3deef03db0f35
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 15:14:02 -0500

    user

--- car.sh
+++ car.sh
@@ -1,13 +1,13 @@
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
- yum install nodejs -y &>>/tmp/car.log
- useradd roboshop &>>/tmp/car.log
- curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/car.log
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+ yum install nodejs -y
+ useradd roboshop
+ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
  cd /home/roboshop
- unzip /tmp/cart.zip &>>/tmp/car.log
- mv cart-main cart &>>/tmp/car.log
+ unzip /tmp/cart.zip
+ mv cart-main cart
  cd cart
- npm install &>>/tmp/car.log
- mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/car.log
- systemctl daemon-reload &>>/tmp/car.log
- systemctl start cart  &>>/tmp/car.log
- systemctl enable cart &>>/tmp/car.log
+ npm install
+ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+ systemctl daemon-reload
+ systemctl start cart
+ systemctl enable cart
