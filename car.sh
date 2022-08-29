555f3c3f8246bfbc08d6c7c589fd81b786a5a39b
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 15:23:54 -0500

    user

--- car.sh
+++ car.sh
@@ -23,11 +23,47 @@
      echo FAILURE
    fi
  cd /home/roboshop &>>/tmp/cart.log
+  if [ $? -eq 0 ];then
+    echo SUCCESS
+   else
+     echo FAILURE
+   fi
  unzip /tmp/cart.zip &>>/tmp/cart.log
+  if [ $? -eq 0 ];then
+    echo SUCCESS
+   else
+     echo FAILURE
+   fi
  mv cart-main cart &>>/tmp/cart.log
+ if [ $? -eq 0 ];then
+     echo SUCCESS
+    else
+      echo FAILURE
+    fi
+
  cd cart &>>/tmp/cart.log
+ if [ $? -eq 0 ];then
+     echo SUCCESS
+    else
+      echo FAILURE
+    fi
  npm install &>>/tmp/cart.log
+ if [ $? -eq 0 ];then
+     echo SUCCESS
+    else
+      echo FAILURE
+    fi
  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
+ if [ $? -eq 0 ];then
+     echo SUCCESS
+    else
+      echo FAILURE
+    fi
  systemctl daemon-reload &>>/tmp/cart.log
  systemctl start cart &>>/tmp/cart.log
  systemctl enable cart  systemctl enable cart
+ if [ $? -eq 0 ];then
+     echo SUCCESS
+    else
+      echo FAILURE
+    fi
