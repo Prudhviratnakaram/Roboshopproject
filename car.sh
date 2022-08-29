6b349ca690e4bc5ea5c6d29102580dd3e570a744
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 14:39:54 -0500

    user

--- car.sh
+++ car.sh
@@ -1,82 +1,13 @@
-
- curl -sL https://rpm.nodesource.com/setup_lts.x
- rm -rf
-
-if [ $? -eq 0 ];then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-fi
-
- yum install nodejs -y >>/tmp/car.log
-if [ $? -eq 0 ];then
-   echo -e "\e[32SUCCESS\e[0m"
-else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- useradd roboshop >>/tmp/car.log
- if [ $? -eq 0 ];then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  >>/tmp/car.log
- if [ $? -eq 0 ];then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- cd /home/roboshop >>/tmp/car.log
- if [ $? -eq 0 ];then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- unzip /tmp/cart.zip >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- mv cart-main cart >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- cd cart >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- npm install >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- systemctl daemon-reload >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- systemctl start cart >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-  fi
- systemctl enable cart >>/tmp/car.log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32SUCCESS\e[0m"
- else
-   echo -e "\e[31FAILURE\e[0m"
-fi
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+ yum install nodejs -y
+ useradd roboshop
+ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+ cd /home/roboshop
+ unzip /tmp/cart.zip
+ mv cart-main cart
+ cd cart
+ npm install
+ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+ systemctl daemon-reload
+ systemctl start cart
+ systemctl enable cart
