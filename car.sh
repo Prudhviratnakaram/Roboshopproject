4d1d72f9c288b266b73cf751c4a267077afdd4e5
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 15:22:15 -0500

    user

--- car.sh
+++ car.sh
@@ -4,15 +4,30 @@
   else
     echo FAILURE
   fi
- yum install nodejs -y
- useradd roboshop
- curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
- cd /home/roboshop
- unzip /tmp/cart.zip
- mv cart-main cart
- cd cart
- npm install
- mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
- systemctl daemon-reload
- systemctl start cart
- systemctl enable cart
+ yum install nodejs -y &>>/tmp/cart.log
+  if [ $? -eq 0 ];then
+    echo SUCCESS
+   else
+     echo FAILURE
+   fi
+ useradd roboshop &>>/tmp/cart.log
+  if [ $? -eq 0 ];then
+    echo SUCCESS
+   else
+     echo FAILURE
+   fi
+ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
+  if [ $? -eq 0 ];then
+    echo SUCCESS
+   else
+     echo FAILURE
+   fi
+ cd /home/roboshop &>>/tmp/cart.log
+ unzip /tmp/cart.zip &>>/tmp/cart.log
+ mv cart-main cart &>>/tmp/cart.log
+ cd cart &>>/tmp/cart.log
+ npm install &>>/tmp/cart.log
+ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
+ systemctl daemon-reload &>>/tmp/cart.log
+ systemctl start cart &>>/tmp/cart.log
+ systemctl enable cart  systemctl enable cart
