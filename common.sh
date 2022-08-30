a5de5dc516dd4f05ce8762a40200ad4503515cc0
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-30 11:14:16 -0500

    user

--- common.sh
+++ common.sh
@@ -32,3 +32,21 @@ Download()
 }
 log=/tmp/${component}.log
 rm -f${log}
+Java()
+{
+   yum install maven -y
+   useradd roboshop
+   StatusCheck
+   cd /home/roboshop
+   rm -rf shipping
+   curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+   StatusCheck
+   unzip -o /tmp/shipping.zip
+   mv shipping-main shipping
+   cd shipping
+   mvn clean package
+   mv target/shipping-1.0.jar shipping.jar
+   mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+   StatusCheck
+   systemctl daemon-reload && systemctl start shipping && systemctl enable shipping
+}
