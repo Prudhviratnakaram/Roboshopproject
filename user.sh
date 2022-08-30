10547b3259b18ebdc529184772381baca550d64b
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-30 10:16:18 -0500

    user

--- user.sh
+++ user.sh
@@ -1,15 +1,14 @@
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
- yum install nodejs -y
-  useradd roboshop
-  curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+  source common.sh
+  component= user
+  Nodejs
+  Download
   cd /home/roboshop
   rm -rf user
   unzip -o /tmp/user.zip
   mv user-main user
   cd /home/roboshop/user
+  echo "installing the npm packages"
   npm install
   mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 
-  systemctl daemon-reload
-  systemctl start user
-  systemctl enable user
+  systemctl daemon-reload &&  systemctl start user &&  systemctl enable user
