c8f45d36a483018dfd16528f0faf223857920509
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-30 18:10:21 -0500

    user

--- redis.sh
+++ redis.sh
@@ -8,5 +8,8 @@ component=redis
  echo "install redis"
  yum install redis-6.2.7 -y &>>${log}
  StatusCheck
+ echo "updating the redis listen address"
+ sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log}
+ StatusCheck
  systemctl enable redis && systemctl start redis &>>${log}
  StatusCheck
