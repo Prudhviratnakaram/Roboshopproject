ff3f77073d4bda1e1163d14183d83ad3b2f7ba9a
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 19:48:45 -0500

    user

--- redis.sh
+++ redis.sh
@@ -1,6 +1,12 @@
-
- curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+#!/usr/bin/bash
+source common.sh
+component=redis
+ echo "set up yum repo"
+ curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${log}
+ StatusCheck
  rm -rf redis
- yum install redis-6.2.7 -y
- systemctl enable redis
- systemctl start redis
+ echo "install redis"
+ yum install redis-6.2.7 -y &>>${log}
+ StatusCheck
+ systemctl enable redis && systemctl start redis &>>${log}
+ StatusCheck
