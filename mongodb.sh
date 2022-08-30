6598ee5d07f355251cb54b9aa8a4534b8c3f2dc9
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-29 19:24:47 -0500

    user

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,25 @@
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-
+#!/usr/bin/bash
+source common.sh
+component=mongodb
+echo "set up m repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${log}
+StatusCheck
+echo "install mongodb"
  yum install -y mongodb-org
- systemctl enable mongod
- systemctl start mongod
-
+ StatusCheck
+ echo "Enable mongodb"
+ systemctl enable mongod && systemctl start mongod &>>${log}
+StatusCheck
   systemctl restart mongod
 
-   curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+  Download
 
    cd /tmp
-   unzip -o mongodb.zip
-   cd mongodb-main
-   mongo < catalogue.js
-   mongo < users.js
+   echo "Extract the schema"
+   unzip -o mongodb.zip &>>${log}
+
+   echo "load schema"
+  cd mongodb-main &&  mongo < catalogue.js &&  mongo < users.js &>>${log}
 
 
 
