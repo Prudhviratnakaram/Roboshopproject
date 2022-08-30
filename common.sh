f7c747d4c901d9c7d1cef4a84a4989510fd8e47a
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-08-30 11:21:03 -0500

    user

--- common.sh
+++ common.sh
@@ -43,9 +43,7 @@ Java()
    unzip -o /tmp/shipping.zip
    mv shipping-main shipping
    cd shipping
-   mvn clean package
-   mv target/shipping-1.0.jar shipping.jar
-   mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+   mvn clean package && mv target/shipping-1.0.jar shipping.jar && mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service &>>{log}
    StatusCheck
    systemctl daemon-reload && systemctl start shipping && systemctl enable shipping
 }
