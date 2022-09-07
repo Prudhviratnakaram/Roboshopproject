0369e398997ccf1789b3a4809e97f01bb1f0c74a
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-07 16:02:38 -0500

    user

--- car.sh
+++ car.sh
@@ -3,13 +3,10 @@ component=cart
 source common.sh
 
 Nodejs
-
-
-
  echo configuring the systemd service
- mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/car.log &&  systemctl daemon-reload &>>/tmp/car.log
+ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log &&  systemctl daemon-reload &>>/tmp/cart.log
  StatusCheck
 
  echo starting the cart service
- systemctl start cart &>>/tmp/car.log && systemctl enable cart &>>/tmp/car.log
+ systemctl start cart &>>/tmp/cart.log && systemctl enable cart &>>/tmp/cart.log
  StatusCheck
