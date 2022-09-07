cc6181f05d25332ecfac9d95fb34011e0da48abd
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-07 12:52:58 -0500

    user

--- mysql.sh
+++ mysql.sh
@@ -3,8 +3,8 @@
  yum install mysql-community-server -y
  systemctl enable mysqld
  systemctl start mysqld
- deafault_password=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print$NF}')
-echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql -uroot -p$(deafault_password)
+ DEAFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print$NF}')
+echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql -uroot -p$(DEAFAULT_PASSWORD)
  mysql -uroot -pRoboShop@1
 #> uninstall plugin validate_password;
  curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
