e4b2bb9d96b6720dcfb0c6a1345d5b34cea15b3e
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-07 16:58:23 -0500

    user

--- car.sh
+++ car.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/bash
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
- yum install nodejs -y
- id roboshop
+  echo "setting nodejs repos"
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
+   echo "installing node js"
+ yum install nodejs -y &>>/tmp/cart.log
+
+ id roboshop &>>/tmp/cart.log
    if [ $? -ne 0 ];then
+      echo "adding the roboshopsuer"
     echo "adding the roboshopsuer"
     useradd roboshop
    StatusCheck
    fi
- curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
- cd /home/roboshop
- unzip /tmp/cart.zip
- mv cart-main cart
- cd cart
- npm install
- mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
- systemctl daemon-reload
- systemctl start cart
- systemctl enable cart
+    echo Downloading the App contennt
+ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
+ cd /home/roboshop &>>/tmp/cart.log
+    echo extracting the archive
+ unzip -o /tmp/cart.zip &>>/tmp/cart.log
+ mv cart-main cart &>>/tmp/cart.log
+ cd cart &>>/tmp/cart.log
+   echo installing the nodejs dependencies
+ npm install &>>/tmp/cart.log
+ echo configuring the systemd service
+ mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
+ systemctl daemon-reload &>>/tmp/cart.log
+ systemctl start cart &>>/tmp/cart.log
+ systemctl enable cart &>>/tmp/cart.log
