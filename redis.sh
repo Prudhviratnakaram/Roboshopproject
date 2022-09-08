63e68a4e8cdf124b91d95895db5bb0122cb12fe4
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-08 15:23:42 -0500

    user

--- redis.sh
+++ redis.sh
@@ -4,6 +4,8 @@ source common.sh
 echo setup mrepo
  curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${log}
  StatusCheck
+  dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
+  dnf module enable redis:remi-6.2 -y &>>${log}
  echo install the redis &>>${log}
  yum install redis-6.2.7 -y &>>${log}
  StatusCheck
